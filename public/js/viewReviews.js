f4cf91b0966c93fb92b4bacfbdcead7cd2772af1
Chirs Greene <chigreenwork@gmail.com>
2023-10-27 20:59:36 -0400

    makes it so review can be toggled in and out from product div

--- public/js/viewReviews.js
+++ public/js/viewReviews.js
@@ -1,3 +1,16 @@
-function showReviews() {
-    document.getElementById('reviewDiv')
-}
+
+document
+    .getElementById('showBtn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        document.getElementById("reviewDiv").style.display = "block";
+        document.querySelector(".item").classList.remove("item-full-width");
+    });
+
+document
+    .getElementById('hideBtn')
+    .addEventListener('click', function (event) {
+        event.preventDefault()
+        document.getElementById("reviewDiv").style.display = "none";
+        document.querySelector(".item").classList.add("item-full-width");
+    });
