196dc376b5edfd2c61493b9405254cd783628b6c
Chirs Greene <chigreenwork@gmail.com>
2023-10-29 13:33:34 -0400

    works on population the divs with items

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,24 @@
+const FallItem = require('../models/fall');
+
 const router = require('express').Router();
 
 
-router.get('/', (req, res) => {
-    res.render('login',{
-        loggedIn: req.session.loggedIn,
-    }); 
+router.get('/', async (req, res) => {
+    try { 
+        const itemData = await FallItem.findAll()
+        const items = itemData.map((item) => {
+            return item.get({
+                plain: true
+            })
+        })
+        res.render('login', {
+            items,
+            loggedIn: req.session.loggedIn,
+        }); 
+    } catch (err) {
+        res.status(500).json(err)
+    }
+    
 });
 
 module.exports = router;
