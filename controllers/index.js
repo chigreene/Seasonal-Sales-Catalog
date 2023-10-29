3afbb2ee97e3f04e3100a1d173b36ba2a600445b
Damion Morgan <only4school5680@gmail.com>
2023-10-28 21:07:10 -0400

    fixed route name

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,10 @@
 
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
-const seasonRoutes=require('./season-routes.js')
 const apiRoutes=require('./api')
 
 router.use('/api',apiRoutes)
 router.use('/', homeRoutes);
-router.use('/season',seasonRoutes)
+
 
 module.exports = router;
--- controllers/season-routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-// add parameter so it show the reason with route in url router.get('/:id'...
-router.get('/', (req, res) => {
-    res.render('seasons');
-});
-
-module.exports = router;
