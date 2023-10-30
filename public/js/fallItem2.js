00a0455dbf95e2fa6938eb20ef7d13b789c9342b
Chirs Greene <chigreenwork@gmail.com>
2023-10-30 07:23:03 -0400

    clicking a new picture removes the other item  review from the review div

--- public/js/fallItem2.js
+++ public/js/fallItem2.js
@@ -2,6 +2,7 @@ document
   .getElementById("fallItem2")
   .addEventListener("click", function (event) {
     event.preventDefault();
+    document.getElementById("review1").style.display = "none";
     document.getElementById("sampleClick").style.display = "block";
     document.getElementById("review2").style.display = "block";
     document.getElementById("itemClick").classList.remove("item-full-width");
