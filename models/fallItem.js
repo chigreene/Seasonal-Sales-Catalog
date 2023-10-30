d6fa9cb261cad0d96c4a13f8a9fc80010a19638f
Chirs Greene <chigreenwork@gmail.com>
2023-10-29 21:03:10 -0400

    working on having individual items open differnet reviews section

--- models/fallItem.js
+++ models/fallItem.js
@@ -4,30 +4,32 @@ const sequelize = require('../config/connection')
 class FallItem extends Model { };
 
 FallItem.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        filename: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'fall_item'
-    }
-
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    cssItemId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    filename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "fall_item",
+  }
 );
 
 module.exports = FallItem;
