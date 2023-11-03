565773be50dc58556b2fb5a1d5c59fe22bd3ae80
BeginnerLevelUP <132778704+BeginnerLevelUP@users.noreply.github.com>
2023-11-02 23:57:15 -0400

    Login fix (#15)
    * creates sepearte forms for both login and creating accounts
    
    * user can now enter email OR password instead of both
    
    * now user can create and login into an account from seperate fields
    
    * adds middleware for home routes and creates a recovery route
    
    * working on user route
    
    * fixed
    
    * progess
    
    * everything should be working now just need to test
    
    * struggle is over temporarily

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
+const { Op } = require('sequelize');
 router.post('/', async (req, res) => {
   try {
     const dbUserData = await User.create({
@@ -20,13 +20,16 @@ router.post('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+// Login
 router.post('/login', async (req, res) => {
   try {
     const dbUserData = await User.findOne({
       where: {
-        email: req.body.email,
-      },
+        [Op.or]: [
+          { email: req.body.both },
+          { user_name: req.body.both }
+        ]
+      }
     });
 
     if (!dbUserData) {
@@ -69,5 +72,76 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
+
+router.post('/recover', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        [Op.or]: [
+          { email: req.body.both },
+          { user_name: req.body.both }
+        ]
+      }
+    });
+
+    if (!dbUserData) {
+      res.status(400).json({ message: 'User not found. Please check your email or username.' });
+      return;
+    }
+
+    res.status(200).json({ recover: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post('/recover/password', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        [Op.or]: [
+          { email: req.body.both },
+          { user_name: req.body.both }
+        ]
+      }
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (validPassword) {
+      res
+        .status(500)
+        .json({ message: 'Cannot use the current password' });
+      return;
+    }
+
+
+    dbUserData.password = req.body.password;
+    await dbUserData.save();
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
+
 
 module.exports = router;
