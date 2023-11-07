1f671bb155b92628236c26ab6df84c0dc2068529
Damion Morgan <only4school5680@gmail.com>
2023-11-06 20:13:23 -0500

    edited public js files

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,26 @@
 // Select all the needed DOM elements
 
+
+
 // form fields
+const both_Em_Us = document.querySelector("#both_Em_Us");
 const userNameInput = document.querySelector("#userName");
 const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
-
+const loginPasswordInput = document.querySelector("#password");
+const newPasswordInput = document.querySelector("#newPassword");
+const recover_Em_Us = document.querySelector('#recover_Us_em')
+const updatePassword = document.querySelector('#updatePassword')
+const matchingPassword = document.querySelector('#matchingPassword')
 // buttons
 const createButton = document.querySelector("#create");
 const loginButton = document.querySelector("#login");
 const logoutButton = document.querySelector("#logout");
+const recoverButton = document.querySelector('#recover')
+const updateButton = document.querySelector('#update')
+//Div
+const passwordRecovDiv = document.querySelector('#passwordRecovDiv')
+
+// Setting States
 
 
 //functions
@@ -17,7 +29,7 @@ const create = async (event) => {
 
     const username = userNameInput.value.trim();
     const email = emailInput.value.trim();
-    const password = passwordInput.value.trim();
+    const password = newPasswordInput.value.trim();
 
     if (username && email && password) {
         const response = await fetch('/api/users', {
@@ -36,16 +48,16 @@ const create = async (event) => {
 
 
 
-const login= async (event) => {
+const login = async (event) => {
     event.preventDefault();
+    console.log('clicked')
+    const both = both_Em_Us.value.trim();
+    const password = loginPasswordInput.value.trim();
 
-    const email = emailInput.value.trim();
-    const password = passwordInput.value.trim();
-
-    if (email && password) {
+    if (both && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ both, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
@@ -59,18 +71,83 @@ const login= async (event) => {
 
 const logout = async (event) => {
     event.preventDefault();
-        const response = await fetch('/api/users/logout', {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out.');
+    }
+}
+
+const recover = async (event) => {
+    event.preventDefault();
+
+    both = recover_Em_Us.value.trim();
+
+    if (both) {
+        const response = await fetch('/api/users/recover', {
             method: 'POST',
+            body: JSON.stringify({ both }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
+            const responseData = await response.json();
+            if (responseData.recover === true) {
+                console.log('Recovery process initiated');
+                passwordRecovDiv.style.display = 'block';
+                updateButton.style.display = 'block'
+                recoverButton.style.display = 'none'
+                recover_Em_Us.disabled = true;
+            } else {
+                alert('User not found.');
+            }
+        } else {
+            alert('Failed to retrieve user data.');
+        }
+    }
+};
+
+
+const recoverPwd = async (event) => {
+    event.preventDefault();
+    const both = recover_Em_Us.value.trim();
+    const password = updatePassword.value.trim()
+    if (both && password) {
+        const response = await fetch('/api/users/recover/password', {
+            method: 'POST',
+            body: JSON.stringify({ both, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            console.log('password Changed')
             document.location.replace('/');
         } else {
-            alert('Failed to log out.');
+            updatePassword.value = ''
+            console.log('cannot use the same password at before retry')
         }
+    }
+};
+
+
+if (loginButton) {
+    loginButton.addEventListener('click', login)
+}
+if (logoutButton) {
+    logoutButton.addEventListener('click', logout)
+}
+if (createButton) {
+    createButton.addEventListener('click', create)
 }
 
-createButton.addEventListener('click',create)
-loginButton.addEventListener('click',login)
-logoutButton.addEventListener('click', logout)
+if (recoverButton) {
+    recoverButton.addEventListener('click', recover)
+}
+if (updateButton) {
+    updateButton.addEventListener('click', recoverPwd)
+}
