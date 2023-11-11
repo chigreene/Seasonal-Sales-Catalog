6f9a9e87832ab56d93ffb2d87588892dfef2d691
Christopher Greene <chigreenwork@gmail.com>
2023-11-11 13:52:51 -0500

    buttons work better and look good but ui for reviews is broken

--- public/js/general.js
+++ public/js/general.js
@@ -1,3 +1,27 @@
-document.getElementById("seasonsButton").addEventListener("click", function () {
-  window.location.href = "/seasons";
+const seasonsButton = document.getElementById("seasonsButton");
+
+if (seasonsButton) {
+  seasonsButton.addEventListener("click", function () {
+    window.location.href = "/seasons";
+  });
+}
+
+document.getElementById("viewProfile").addEventListener("click", function () {
+  window.location.href = "/u";
+});
+
+document.getElementById("fallButton").addEventListener("click", function () {
+  window.location.href = "/seasons/fall";
+});
+
+document.getElementById("winterButton").addEventListener("click", function () {
+  window.location.href = "/seasons/winter";
+});
+
+document.getElementById("springButton").addEventListener("click", function () {
+  window.location.href = "/seasons/spring";
+});
+
+document.getElementById("summerButton").addEventListener("click", function () {
+  window.location.href = "/seasons/summer";
 });
