daaad9405c6d987c375a87ac7ceed37312f25aa5
Christopher Greene <chigreenwork@gmail.com>
2023-11-11 17:24:06 -0500

    adds single season page that shows reviews for each product

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,6 @@ const User = require("../models/user");
 const Item = require("../models/item");
 const Review = require("../models/review");
 
-
 const router = require("express").Router();
 
 // Function to get reviews by item_id
@@ -18,52 +17,47 @@ async function getReviewsByItemId(itemId) {
 // set up middleware
 const checkLoginStatus = (req, res, next) => {
   if (!req.session.loggedIn) {
-    return res.redirect('/login');
-  }else{
+    return res.redirect("/login");
+  } else {
     next();
   }
 };
 
-//change login Page 
-const seasonSwtich= async ()=>{
+//change login Page
+const seasonSwtich = async () => {
   //my seasonswitch js will be usefull
   // everything would be based on the date
   //I would need to create a date object then logic to get season it is then
   //now im able to make the datebase query based on the season once we have the season
-  // make a query on the holiday send that all to the login route 
-  // then once it's inside the data attribute use js to make img tags for them 
+  // make a query on the holiday send that all to the login route
+  // then once it's inside the data attribute use js to make img tags for them
+};
 
-  
-}
-
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   const currentDate = new Date();
 
   try {
-    res.render('login', {
+    res.render("login", {
       loggedIn: req.session.loggedIn,
     });
 
-    if(req.session.loggedIn){
-      res.redirect('/seasons')
+    if (req.session.loggedIn) {
+      res.redirect("/seasons");
     }
   } catch (err) {
-    res.status(500).json('unable to fufill request')
+    res.status(500).json("unable to fufill request");
   }
-
 });
 
-router.get('/login/recover', (req, res) => {
+router.get("/login/recover", (req, res) => {
   try {
-    res.status(200).render('loginRecover')
+    res.status(200).render("loginRecover");
+  } catch (err) {
+    res.status(500).json("unable to fufill request");
   }
-  catch (err) {
-    res.status(500).json('unable to fufill request')
-  }
-
 });
 
-router.get('/seasons', checkLoginStatus, async (req, res) => {
+router.get("/seasons", checkLoginStatus, async (req, res) => {
   try {
     const itemData = await Item.findAll({});
     const items = itemData.map((item) => item.get({ plain: true }));
@@ -107,7 +101,7 @@ router.get('/seasons', checkLoginStatus, async (req, res) => {
     res.status(500).json(err);
   }
   if (!req.session.loggedIn) {
-    res.redirect('/login')
+    res.redirect("/login");
   }
 });
 
@@ -123,7 +117,9 @@ router.get("/user", async (req, res) => {
 
     const user = userIdData.get({ plain: true });
 
-    res.status(200).render("userPortal", { user, loggedIn: req.session.loggedIn });
+    res
+      .status(200)
+      .render("userPortal", { user, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -143,7 +139,9 @@ router.get("user/:id", async (req, res) => {
     });
     const reviews = reviewData.map((review) => review.get({ plain: true }));
 
-    res.status(200).render("userPortal", { reviews, loggedIn: req.session.loggedIn, });
+    res
+      .status(200)
+      .render("userPortal", { reviews, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -155,7 +153,7 @@ router.get("user/:id", async (req, res) => {
 // ...
 
 // route to get users reviews
-router.get('/user/:id', async (req, res) => {
+router.get("/user/:id", async (req, res) => {
   try {
     const userId = req.params.id;
 
@@ -167,7 +165,9 @@ router.get('/user/:id', async (req, res) => {
     });
     const reviews = reviewData.map((review) => review.get({ plain: true }));
 
-    res.status(200).render('userPortal', { reviews, loggedIn: req.session.loggedIn });
+    res
+      .status(200)
+      .render("userPortal", { reviews, loggedIn: req.session.loggedIn });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -175,7 +175,7 @@ router.get('/user/:id', async (req, res) => {
 });
 
 // Move the catch-all route to the end
-router.get('/seasons/:season', checkLoginStatus, async (req, res) => {
+router.get("/seasons/:season", checkLoginStatus, async (req, res) => {
   try {
     const season = req.params.season;
 
@@ -183,38 +183,29 @@ router.get('/seasons/:season', checkLoginStatus, async (req, res) => {
       where: {
         season: season,
       },
+      include: [
+        {
+          model: Review,
+          attributes: ["username", "review", "createdAt"],
+        },
+      ],
     });
 
     const items = itemData.map((item) => item.get({ plain: true }));
 
-    res.render('oneSeason', { items, season });
+    res.render("singleSeason", { items, season });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
-
-
 });
 
-
-
-// router.get('*', checkLoginStatus, (req, res) => {
-//   try {
-//     res.redirect('/seasons');
-//   } catch (err) {
-//     res.status(404).json('Page not found');
-//   }
-// });
-
-
-router.get('/', checkLoginStatus, (req, res) => {
+router.get("/", checkLoginStatus, (req, res) => {
   try {
-    res.redirect('/login');
+    res.redirect("/login");
   } catch (err) {
-    res.status(404).json('Page not found');
+    res.status(404).json("Page not found");
   }
 });
-
-
 
 module.exports = router;
